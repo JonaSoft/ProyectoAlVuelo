d6864bdc93e2d00dc08f7d6b3c4e185eb6eb3b62
CASA-PORTATIL\Casa <carlosucananbarrera@hotmail.com>
2017-10-06 23:45:24 -0500

    Primer respaldo

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+	//alert("Modulo Web de Vuelos Codeshare..Ya era hora...!!!");
+document.getElementById('winBuscar').addEventListener('click',ventana);
+function ventana(){
+		var ventanaBuscar="width=500, height=300, top=250, left=500, scrollbars=no,toolbars=no";
+		open("protoss.html","CodeShare",ventanaBuscar);
+}
